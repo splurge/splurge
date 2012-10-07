0aef21076cd9f28859be44d7352c8d384d2e739c
William Denton <wtd@pobox.com>
2012-10-07 17:34:33 -0400

    Improve look of demo page and results

--- app/splurge/webapp/static/init.js
+++ app/splurge/webapp/static/init.js
@@ -14,16 +14,17 @@ $(function() {
         $.getJSON(url, function(alldata) {
             console.log(alldata);
             $('#box').empty();
+            $('#original').empty();
             var imgurl = "http://covers.openlibrary.org/b/ISBN/" + alldata.isbn + "-M.jpg";
-            $('#box').append('<img src="' + imgurl + '" height="75" alt="" /><br/>');
-            $('#box').append("<h4>Results for: " + alldata.isbn + "</h4><hr/>");
+            $('#original').append('<div class="cover"><img src="' + imgurl + '" height="100" alt="" /> <br /> ' + alldata.isbn + '</div>');
+            // $('#original').append("<h4>Results for: " + alldata.isbn + "</h4><hr/>");
                     
             for (i = 0; i < alldata.results.length; i++) {
                 var data = alldata.results[i];
                 console.log(data);
                 //$('#box').append(data);
                 var imgurl = "http://covers.openlibrary.org/b/ISBN/" + data[1] + "-M.jpg";
-                $('#box').append('<img src="' + imgurl + '" height="75" alt="" />Rank: <b>' + data[2] + '</b> ISBN:' + data[1] + '<br/>');
+                $('#box').append('<div class="cover"><img src="' + imgurl + '" height="200" alt="" /> <br /> Rank: ' + data[2] + '<br />' + data[1] + '</div>');
             }
         });
     });
